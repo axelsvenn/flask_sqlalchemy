c584b9d7cdb1510be9fd07731f2c40e9a9c50c16
axelsvenn <mrr.axelsven@gmail.com>
2022-04-15 15:09:54 +0300

    class

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
-from data.users import User
+from data.news import News
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
+
+
 def main():
     db_session.global_init("db/blogs.db")
 
-    user = User()
-    user.name = "Пользователь 5"
-    user.about = "биография пользователя 5"
-    user.email = "email5@email.ru"
-    db_sess = db_session.create_session()
-    db_sess.add(user)
-    db_sess.commit()
-    # app.run()
+    app.run()
 
 
 if __name__ == '__main__':
